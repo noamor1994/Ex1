2326617fb4aeb619321c1e8346fc80d88be49ade
noamor1994 <noamor1994@gmail.com>
2021-03-24 20:58:48 +0200

    third

--- equations.py
+++ equations.py
@@ -1,11 +1,12 @@
 def power (X, Y):
     try:
+        X=float(X)
         resultPow= 1
         i=0 
         while (i<Y):
             resultPow=resultPow*X
             i=i+1 
-        return float('%0.6f' % resultPow)
+        return float(resultPow)
     except:
         return 0.0
 
@@ -17,7 +18,7 @@ def factorial (X):
         while (i<X):
             resultFact=resultFact*(i+1)
             i=i+1 
-        return float('%0.6f' % resultFact)
+        return float(resultFact)
     except:
         return 0.0
 
@@ -29,19 +30,22 @@ def exponent (X):
         while (i<70):
             resultExp=resultExp+((power(X,i))/factorial(i))
             i=i+1 
-        return float('%0.6f' % resultExp)
+        return float(resultExp)
     except:
         return 0.0
 
 
 def Ln (X):
     try:
-        Yn= X-1.0
-        YnPlus1= Yn+2*((X-exponent(Yn))/(X+exponent(Yn))) 
-        while (Yn-YnPlus1>0.001 or YnPlus1-Yn>0.001):
-            Yn=YnPlus1
-            YnPlus1= Yn+2*((X-exponent(Yn))/(X+exponent(Yn)))
-        return float('%0.6f' % YnPlus1)
+        if X>0:
+            Yn= X-1.0
+            YnPlus1= Yn+2*((X-exponent(Yn))/(X+exponent(Yn))) 
+            while (Yn-YnPlus1>0.001 or YnPlus1-Yn>0.001):
+                Yn=YnPlus1
+                YnPlus1= Yn+2*((X-exponent(Yn))/(X+exponent(Yn)))
+            return float(YnPlus1)
+        else:
+            return 0.0
     except:
         return 0.0
 
@@ -75,3 +79,20 @@ def calculate (X):
        return float('%0.6f' % result)      
    except:
         return 0.0                               
+    
+    
+x=float(input('>'))
+y=float(input('>'))
+print('')
+print(exponent(x))
+print(Ln(x))
+print(XtimesY(x,y))
+print(sqrt(x,y))
+print(calculate(x))
+print('----------------------------------------------------')
+import math
+print(math.exp(x))
+print('')
+print(x**y)
+print(y**(1/x))
+print(math.exp(x)*(7**x)*(x**-1)*(x**(1/x)))
